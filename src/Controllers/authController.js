774c96bf9c0df401b6d31c7e5a20ca04e44dda57
Terence Faid JABO <faidterence@outlook.com>
2023-11-05 10:02:15 +0200

    Implement user login functionality

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,29 +1,31 @@
 import User from "../Models/UserSchema.js";
 import Doctor from "../Models/DoctorSchema.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { generateToken } from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
   try {
     if (!name || !email || !password || !role || !gender || !photo) {
-      res.status(401).json({ message: "Invalid Inputs" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Inputs" });
     }
-    let user = null;
+
+    let user;
+
     if (role === "patient") {
       user = await User.findOne({ email });
     } else if (role === "doctor") {
       user = await Doctor.findOne({ email });
     }
-    // If user exists
 
     if (user) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
 
-    // hash Password
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
@@ -36,9 +38,7 @@ export const registerUser = async (req, res) => {
         gender,
         role,
       });
-    }
-
-    if (role === "doctor") {
+    } else if (role === "doctor") {
       user = new Doctor({
         name,
         email,
@@ -47,24 +47,55 @@ export const registerUser = async (req, res) => {
         gender,
         role,
       });
+    } else {
+      return res.status(400).json({ success: false, message: "Invalid role" });
     }
 
     await user.save();
 
     res.status(200).json({ success: true, message: "Registration Successful" });
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 };
 
 export const Login = async (req, res) => {
-  const { email, password } = req.body;
+  const { email } = req.body;
   try {
-    if (!email || !password) {
-      res.status(401).json({ message: "Invalid Inputs" });
+    if (!email || !req.body.password) {
+      res.status(401).json({ message: "Invalid Inputs!" });
     }
-    const user = await User.findOne({ email });
+    let user = null;
+    const patient = await User.findOne({ email });
     const doctor = await Doctor.findOne({ email });
-    if (!user || !doctor) {
+    if (patient) user = patient;
+    if (doctor) user = doctor;
+    if (!user)
       res.status(401).json({ success: false, message: "Invalid Credentials" });
+
+    //Compare password with Hashed Password
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordMatch) {
+      res.status(401).json({ success: false, message: "Invalid Cedentials" });
     }
-  } catch (error) {}
+    //Generate Token
+    const token = generateToken(user);
+
+    const { password, role, appointments, ...rest } = user._doc;
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      token,
+      data: { ...rest },
+      role,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+    console.log(error);
+  }
 };
--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../Controllers/authController.js";
+import { Login, registerUser } from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", Login);
 
 export default router;
